415e82985a548441212f06c3e77b31d7f52bc21f
LehaoLin <lehaolin@link.cuhk.edu.cn>
2023-08-31 11:47:52 +0800

    init

---
+++ setup.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm i -g n
+sudo n latest
+
+sudo apt-get install nginx
+
+ssh-keygen
+sudo apt-get install git
+
+sudo sed -i 's/^#*\(ClientAliveInterval\) .*/\1 20/' /etc/ssh/sshd_config
+sudo sed -i 's/^#*\(ClientAliveCountMax\) .*/\1 5/' /etc/ssh/sshd_config
+sudo systemctl restart ssh
